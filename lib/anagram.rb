f7daf2627339f778f4886537e5eada25bb94a922
David <davidongaroh@gmail.com>
2022-08-17 15:55:28 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_writer :word
+    def initialize(word)
+        @word = word
+        
+    end
+    def match(array)
+array.filter{|n| n.split('').sort == @word.split('').sort }
+        
+    end
+end
